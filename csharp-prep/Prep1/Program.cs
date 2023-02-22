182d83386d23f88c0589f09f42c5a4223f63b7fe
amonreis <ammonreis@gmail.com>
2023-02-22 14:46:21 -0300

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Ask the user for their last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        // Display the user name on the screen in this format:
+        // 'last name', 'first name' 'last name'
+       Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}."); 
     }
 }
