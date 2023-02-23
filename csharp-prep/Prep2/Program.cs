fd84b66360b632ef2decd862d5e21db198c07413
amonreis <ammonreis@gmail.com>
2023-02-23 11:24:14 -0300

    Comments added and Stretch chalenges done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,21 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their grade percentage
         Console.Write("What is your grade percentage? ");
         string userInput = Console.ReadLine();
 
+        // Convert the string to an integer number
         int grade = int.Parse(userInput);
-
+        
         string letter = "";
-
+        string sign = "";
+        int last_digit = grade % 10;
+        
+        // Determine the letter grade
         if (grade >= 90)
         {
-            //Console.WriteLine("Your letter grade is 'A' ");
             letter = "A";
         }
         else if (grade >= 80)
         {
-            //Console.WriteLine("Your letter grade is 'B' ");
             letter = "B";
         }
         else if (grade >= 70)
@@ -26,7 +29,7 @@ class Program
             letter = "C";
         }
         else if (grade >= 60 )
-        {
+        {   
             letter = "D";
         }
         else if (grade < 60)
@@ -34,8 +37,32 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your letter grade is {letter}");
+        // Stretch Challenge 1
+        if (last_digit >= 7)
+        {
+           sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
 
+        // Stretch Challenge 2
+        if (grade > 93)
+        {
+            sign = "";
+        }
+
+        // Stretch Challenge 3
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // Display the letter grade for the user to see
+        Console.WriteLine($"Your letter grade is {letter}{sign}.");
+
+        // Inform the user if they passed the course or not
         if (grade >= 70)
         {
             Console.WriteLine("Congratulations on your well-deserved success. You have passed!! Best wishes for your next adventure!.");
