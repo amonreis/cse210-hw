7fcf5369bcdd3a8ef67b072ba71ee1f11c366218
amonreis <ammonreis@gmail.com>
2023-02-23 08:45:53 -0300

    Finished prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,14 +6,14 @@ class Program
     {
         // Ask the user for their first name
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         // Ask the user for their last name
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
         
         // Display the user name on the screen in this format:
         // 'last name', 'first name' 'last name'
-       Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}."); 
+       Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}."); 
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            //Console.WriteLine("Your letter grade is 'A' ");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            //Console.WriteLine("Your letter grade is 'B' ");
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 )
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations on your well-deserved success. You have passed!! Best wishes for your next adventure!.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this course, tutoring is available for an additional fee!");
+        }
     }
 }
