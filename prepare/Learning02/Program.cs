00cedfc3ccb36508f7326618a7e1258f44816321
amonreis <ammonreis@gmail.com>
2024-09-24 23:56:04 -0300

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Missionary Training Center";
+        job1._jobTitle = "Instructor";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "FamilySearch";
+        job2._jobTitle = "Operation Assistant";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume myresume = new Resume();
+        myresume._name = "Amon Reis";
+        
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
+        
     }
 }
